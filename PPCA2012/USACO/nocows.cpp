f2bd23bd303fa7b4b319efd2b2c072b356c9325e
bobchennan <bobchennan@gmail.com>
2012-07-09 21:13:19 +0800

    2012-7-9-evening2

--- PPCA2012/USACO/nocows.cpp
+++ PPCA2012/USACO/nocows.cpp
@@ -9,7 +9,7 @@
 #include <algorithm>
 using namespace std;
 
-int f[201][101][101];//number\height\last ceng
+int f[201][101][129];//number\height\last ceng
 int C[201][201];
 
 int main(){
@@ -20,20 +20,21 @@ int main(){
 	f[1][1][1]=1;
 	for(int i=1;i<=n;++i)
 		C[i][0]=1;
+	C[0][0]=1;
 	for(int i=1;i<=n;++i)
 		for(int j=1;j<=i;++j)
-			C[i][j]=(C[i][j-1]+C[i-1][j-1])%9901;
+			C[i][j]=(C[i-1][j-1]+C[i-1][j])%9901;
 	for(int i=1;i<=n;++i)
 		for(int j=1;j<=k;++j)
-			for(int r=1;r<=i;++r)
+			for(int r=1;r<=min(i,128);++r)
 				if(f[i][j][r]){
-					for(int l=2;l+i<=n&&l<=2*r;l+=2){
+					for(int l=2;l+i<=n&&l<=2*r&&l<=128;l+=2){
 						f[i+l][j+1][l]+=1LL*f[i][j][r]*C[r][l/2]%9901;
 						f[i+l][j+1][l]%=9901;
 					}
 				}
 	int ans=0;
-	for(int i=1;i<=n;++i)
+	for(int i=1;i<=min(n,128);++i)
 		ans=(ans+f[n][k][i])%9901;
 	fprintf(out,"%d\n",ans);
 	return 0;
---
+++ PPCA2012/USACO/zerosum.cpp
@@ -0,0 +1,11 @@
+#include <cstdio>
+#include <string>
+#include <algorithm>
+using namespace std;
+int n;
+int main(){
+	FILE *in=fopen("zerosum.in","r");
+	FILE *out=fopen("zerosum.out","w");
+	fscanf(in,"%d",&n);
+	return 0;
+}
