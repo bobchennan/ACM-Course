73b907f4dcecf7977d318531dde66f58b2b48b9d
bobchennan <bobchennan@gmail.com>
2013-04-05 00:41:26 +0800

    update Compiler

--- Compiler/bin/cnx/syntactic/test.c
+++ Compiler/bin/cnx/syntactic/test.c
@@ -3,11 +3,13 @@ int whattodo(){
   return c;
 }
 
+typedef int abc;
+
 int main()
 {
   int a,x=-2-8+2+8+10;
   int ****y;
-  a = (int**)x;
+  a = (abc)x;
   if (a==0)a=1;else if(a==2)a=3;else a=4;
   for(a=0;a<=2;++a);
   while(a!=0)--a;
